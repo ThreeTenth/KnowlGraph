5663a0ebcf449659c9299a84499dff1b4810ec77
excing <courage.exc@gmail.com>
2021-01-24 00:49:48 +0800

    Update: Added `level` field, record level

--- ent/schema/node.go
+++ ent/schema/node.go
@@ -15,13 +15,14 @@ type Node struct {
 func (Node) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("cover").Optional(),
+		field.Int("level"),
 	}
 }
 
 // Edges of the Node.
 func (Node) Edges() []ent.Edge {
 	return []ent.Edge{
-		edge.From("tag", Tag.Type).Ref("nodes").Unique(),
+		edge.From("tag", Tag.Type).Ref("nodes").Unique().Required(),
 		edge.To("fork", Node.Type).
 			From("form").
 			Unique(),
