bf5abcb2791a7e12f542da74a0af5fdd9f466915
excing <courage.exc@gmail.com>
2021-01-24 00:29:46 +0800

    Add: `ent` tools

---
+++ ent.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"knowlgraph.com/ent"
+)
+
+// WithTx best Practices, reusable function that runs callbacks in a transaction
+func WithTx(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) error {
+	tx, err := client.Tx(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if v := recover(); v != nil {
+			tx.Rollback()
+			panic(v)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			err = errors.Wrapf(err, "rolling back transaction: %v", rerr)
+		}
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		return errors.Wrapf(err, "committing transaction: %v", err)
+	}
+	return nil
+}
