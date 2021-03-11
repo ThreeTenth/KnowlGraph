a54af9b36d0dcabbebca076d7097c17e832cbf6a
excing <courage.exc@gmail.com>
2021-03-11 10:18:14 +0800

    Update: i18n desgin

--- res/js/app.js
+++ res/js/app.js
@@ -1,20 +1,35 @@
 // app.js
 // Created at 03/09/2021
 
-const Home = { template: '<div>This is Home</div>' }
-const Foo = { template: '<div>This is Foo</div>' }
-const Bar = { template: '<div>This is Bar {{ $route.params.id }}</div>' }
+const Home = { inject: ['i18n'], template: '<div>{{ i18n.thisIsHome }}</div>' }
+const Foo = { inject: ['i18n'], template: '<div>{{ i18n.thisIsFoo }}</div>' }
+const Bar = { inject: ['i18n'], template: '<div>{{ i18n.thisIsBar }}: {{ $route.params.id }}</div>' }
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', name: 'home', component: Home },
-    { path: '/my', name: 'foo', component: Foo },
-    { path: '/drafts/:id', name: 'bar', component: Bar }
+    { path: '/my', name: 'my', component: Foo },
+    { path: '/drafts/:id', name: 'drafts', component: Bar }
   ]
 })
 
 new Vue({
+  provide: {
+    i18n,
+  },
+  data: () => ({
+    i18n,
+  }),
   router,
-  template: fgm_app
+  template: fgm_app,
+  methods: {
+    onToggle: function () {
+      if (i18n.language === 'en') {
+        Object.assign(i18n, zh)
+      } else {
+        Object.assign(i18n, en)
+      }
+    }
+  }
 }).$mount('#app')
