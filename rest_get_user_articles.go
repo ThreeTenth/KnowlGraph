66e66afce91334238f75bbc0add97b327ecd4cab
excing <courage.exc@gmail.com>
2021-04-01 22:28:55 +0800

    Update: changed article list struct

--- rest_get_user_articles.go
+++ rest_get_user_articles.go
@@ -10,14 +10,14 @@ import (
 )
 
 // Articles are used to sort articles
-type Articles []*ent.Asset
+type Articles []*ent.Article
 
 func (d Articles) Len() int { return len(d) }
 
 func (d Articles) Less(i, j int) bool {
-	return d[i].Edges.Article.Edges.Versions[0].CreatedAt.
+	return d[i].Edges.Versions[0].CreatedAt.
 		After(
-			d[j].Edges.Article.Edges.Versions[0].CreatedAt)
+			d[j].Edges.Versions[0].CreatedAt)
 }
 
 func (d Articles) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
@@ -39,18 +39,25 @@ func getUserArticles(c *Context) error {
 		Where(user.ID(_userID.(int))).
 		QueryAssets().
 		Where(asset.StatusEQ(_query.Status)).
-		WithArticle(func(aq *ent.ArticleQuery) {
-			aq.WithVersions(func(vq *ent.VersionQuery) {
-				if _query.Lang != "" {
-					vq = vq.Where(version.Lang(_query.Lang))
-				}
-				vq.Order(ent.Desc(version.FieldCreatedAt))
-			})
+		QueryArticle().
+		WithVersions(func(vq *ent.VersionQuery) {
+			if _query.Lang != "" {
+				vq = vq.Where(version.Lang(_query.Lang))
+			}
+			vq.Order(ent.Desc(version.FieldCreatedAt))
 		}).
 		All(ctx)
 
 	if err != nil {
 		return c.BadRequest(err.Error())
+	}
+
+	for _, v := range _articles {
+		_versions := make([]*ent.Version, 0)
+		if 0 < len(v.Edges.Versions) {
+			_versions = v.Edges.Versions[:1]
+		}
+		v.Edges.Versions = _versions
 	}
 
 	sort.Sort(Articles(_articles))
