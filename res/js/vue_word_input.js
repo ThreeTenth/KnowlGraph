d4516d70d25272681e1de8cf578b7797053fd814
excing <courage.exc@gmail.com>
2021-04-02 12:53:16 +0800

    Update: 针对用户正在使用中文输入法时的删除按键处理

--- res/js/vue_word_input.js
+++ res/js/vue_word_input.js
@@ -15,22 +15,21 @@ Vue.component('words-input', {
       input: "",
       items: [],
       state: WordStateMayDel,
+      __isDelProcess: false,
     }
   },
-
-  created() { console.log(this.$props) },
-  methods: {
-    onWordChanged: function () {
-      if ("" == this.input) {
+  watch: {
+    input: function (val, oldVal) {
+      if ("" == val) {
         if (WordStateSafe == this.state) {
           this.state = WordStateIdle
         }
         this.auto = false
       } else if (this.words) {
         this.state = WordStateSafe
-        if (2 <= this.input.length) {
+        if (2 <= val.length) {
           const items = []
-          const regex = new RegExp('^' + this.input)
+          const regex = new RegExp('^' + val)
           this.words.forEach(item => {
             if (regex.test(item)) {
               items.push(item)
@@ -41,6 +40,31 @@ Vue.component('words-input', {
         } else {
           this.auto = false
         }
+      }
+    },
+  },
+
+  created() { console.log(this.$props) },
+  methods: {
+    onKeyup: function (event) {
+      if ("Process" === event.key) {
+        // 针对中文输入法的特殊处理
+        // 在进入中文输入时，
+        // vue 无法观测到正在输入的内容变化，
+        // 但可以监听到按键变化。
+        // 所以如果当前内容为空，
+        // 但用户正在使用中文输入法输入内容，
+        // 如果使用了删除键，
+        // 这个删除事件就会被捕获，
+        // 那么就会出现错误的删除操作。
+        // 在 Vue 中，如果用户正在进行中文输入，
+        // 那么每次按键事件都被前后触发两次，
+        // 第一次是 key 始终为 "Process" 的按键事件，
+        // 第二次是正常的按键事件，
+        // 通过这个，可以过滤不需要的按键。
+        // 在这里，就是删除键。
+
+        this.$data.__isDelProcess = true
       }
     },
     onRemoveWord: function (word) {
@@ -73,6 +97,10 @@ Vue.component('words-input', {
       this.state = WordStateMayDel
     },
     onDel: function () {
+      if (this.$data.__isDelProcess) {
+        this.$data.__isDelProcess = false
+        return
+      }
       switch (this.state) {
         case WordStateIdle:
           this.state = WordStateMayDel
