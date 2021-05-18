90b6189067fc2272642412b4c122bd190cdc9d64
excing <courage.exc@gmail.com>
2021-05-18 23:31:21 +0800

    Update: autocomplet component

--- res/js/vue_autocomplet.js
+++ res/js/vue_autocomplet.js
@@ -8,7 +8,16 @@ Vue.component('autocomplet', {
       input: "",
       items: [],
       auto: false,
+      pos: 'down',
     }
+  },
+
+  mounted() {
+    this.__update()
+  },
+
+  activated() {
+    this.__update()
   },
 
   methods: {
@@ -23,7 +32,7 @@ Vue.component('autocomplet', {
     onKeyup() {
       this.auto = "" != this.input
 
-      if (!this.auto) return
+      // if (!this.auto) return
 
       const items = []
       const regex = new RegExp('^' + this.input)
@@ -39,7 +48,29 @@ Vue.component('autocomplet', {
       });
       this.items = items
       this.auto = 0 != items.length
-    }
+
+      this.__update()
+    },
+
+    __update() {
+      let len = this.items.length
+
+      if (0 == len ) return
+
+      let autocomplete = this.$refs.autocomplete
+      let top = autocomplete.getBoundingClientRect().top
+      let bottom = autocomplete.getBoundingClientRect().bottom
+
+      let preHeight = len * 30
+
+      console.log(top, bottom, preHeight)
+
+      if (preHeight < bottom) {
+        this.pos = "down"
+      } else {
+        this.pos = "up"
+      }
+    },
   },
 
   template: com_autocomplet,
