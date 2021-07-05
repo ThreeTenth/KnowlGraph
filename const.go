2c3226afbf00694dae755881211e6ab6b5bfc4a2
excing <courage.exc@gmail.com>
2021-07-05 23:47:53 +0800

    Update version name

--- const.go
+++ const.go
@@ -4,9 +4,9 @@ import "time"
 
 const (
 	// Version is current version number
-	Version = 6
+	Version = 7
 	// VersionName is current version name
-	VersionName = "0.0.6"
+	VersionName = "0.0.7"
 	// DBVersion is current database version number
 	DBVersion = 1
 )
