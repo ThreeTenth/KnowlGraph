f33497490bd9d44e5fa48a2f6041c884eeff66bd
excing <courage.exc@gmail.com>
2021-08-11 23:12:25 +0800

    Update: overlay component hidden scroll

--- res/js/components/commons/vue_overlay.js
+++ res/js/components/commons/vue_overlay.js
@@ -2,12 +2,21 @@
 
 Vue.component('overlay', {
 
+  data: function () {
+    return {
+      body_overflow_y: 'scroll',
+    }
+  },
+
   created() {
+    if (document.body.style['overflow-y']) {
+      this.body_overflow_y = document.body.style['overflow-y']
+    }
     document.body.style.overflow = 'hidden'
   },
 
   beforeDestroy() {
-    document.body.style['overflow-y'] = 'scroll'
+    document.body.style['overflow-y'] = this.body_overflow_y
   },
 
   template: com_overlay,
