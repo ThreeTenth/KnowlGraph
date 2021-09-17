d234d1f80464326c48b520bced340a52d3e3b0a8
excing <courage.exc@gmail.com>
2021-09-17 12:06:55 +0800

    Update: add location info to the terminal

---
+++ ent/schema/location.go
@@ -0,0 +1,49 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Location holds the schema definition for the Location entity.
+// The location information must not contain country and province information.
+type Location struct {
+	ent.Schema
+}
+
+// Fields of the Location.
+func (Location) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("city").
+			Optional(),
+		field.Float("lat").
+			Optional().
+			SchemaType(map[string]string{
+				dialect.MySQL:    "decimal(8,6)", // Override MySQL.
+				dialect.Postgres: "numeric",      // Override Postgres.
+			}),
+		field.Float("long").
+			Optional().
+			SchemaType(map[string]string{
+				dialect.MySQL:    "decimal(9,6)", // Override MySQL.
+				dialect.Postgres: "numeric",      // Override Postgres.
+			}),
+		field.Time("created_at").
+			Default(time.Now),
+	}
+}
+
+// Edges of the Location.
+func (Location) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("registrant", Terminal.Type).
+			Ref("registration").
+			Unique(),
+		edge.From("terminal", Terminal.Type).
+			Ref("locations"),
+	}
+}
--- ent/schema/terminal.go
+++ ent/schema/terminal.go
@@ -20,6 +20,7 @@ func (Terminal) Fields() []ent.Field {
 		field.String("ua"),
 		field.Time("created_at").
 			Default(time.Now).
+			Optional().
 			StructTag(`json:"-"`),
 		// field.String("cred_id"),
 		// field.String("public_key"),
@@ -29,7 +30,14 @@ func (Terminal) Fields() []ent.Field {
 
 // Edges of the Terminal.
 func (Terminal) Edges() []ent.Edge {
+	// 终端的地理位置信息和创建时间默认不向客户端显示，需要得到用户的授权，才能显示。
 	return []ent.Edge{
+		edge.To("registration", Location.Type).
+			Unique().
+			StructTag(`json:"-"`),
+		edge.To("locations", Location.Type).
+			StructTag(`json:"-"`),
+
 		edge.From("user", User.Type).
 			Ref("terminals").
 			Unique().
