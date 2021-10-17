4f032f9f427cbabfc499397dac1deab40e18d1bd
excing <courage.exc@gmail.com>
2021-10-17 11:25:42 +0800

    Update rest.js

--- res/js/utils/rest.js
+++ res/js/utils/rest.js
@@ -1,5 +1,49 @@
 // rest.js
 
+function putMakeCredential(challenge, state, success, failure) {
+  axios({
+    method: "PUT",
+    url: queryRestful("/v1/account/t/makeCredential", { state: state, challenge: challenge })
+  }).then(success).catch(failure)
+}
+
+function getScanChallenge(challenge, success, failure) {
+  axios({
+    method: "get",
+    url: queryRestful("/v1/account/t/scanChallenge", { challenge: challenge }),
+  }).then(success).catch(failure)
+}
+
+function postAuthorizeTerminal(challenge, onlyOnce, success, failure) {
+  axios({
+    method: "post",
+    url: queryRestful("/v1/account/t/authorize", { challenge: challenge, onlyOnce: onlyOnce }),
+  }).then(success).catch(failure)
+}
+
+function cancelActivateTerminal(challenge, success, failure) {
+  axios({
+    method: "delete",
+    url: queryRestful("/v1/account/t/activate", { challenge: challenge }),
+  }).then(success).catch(failure)
+}
+
+function postActivateTerminal(challenge, success, failure) {
+  axios({
+    method: "post",
+    url: queryRestful("/v1/account/t/activate", { challenge: challenge }),
+  }).then(success).catch(failure)
+}
+
+function postAuthorizeTerminal(challenge, onlyOnce, success, failure) {
+  axios({
+    method: "post",
+    url: queryRestful("/v1/account/t/authorize", { challenge: challenge, onlyOnce: onlyOnce }),
+  }).then(success).catch(failure)
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
 function queryPage(url, data = undefined) {
   url = window.location.origin + url
   return encodeQueryData(url, data)
@@ -27,5 +71,5 @@ function encodeQueryData(url, data = undefined) {
 }
 
 function getStatus4Error(err) {
-  return err.response ? err.response.status : 9999 
+  return err.response ? err.response.status : 9999
 }
