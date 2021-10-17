b37e8cd237673d90b3170faf5636354e4dcb45a8
excing <courage.exc@gmail.com>
2021-10-17 11:25:03 +0800

    Fix: When delete another terminal, the token that causes the currently operating terminal be deleted incorrectly

--- rest_delete_terminal.go
+++ rest_delete_terminal.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-redis/redis/v8"
 	"knowlgraph.com/ent/terminal"
 	"knowlgraph.com/ent/user"
 )
@@ -17,9 +18,26 @@ func deleteTerminal(c *Context) error {
 	userID, _ := c.Get(GinKeyUserID)
 	token := getRequestToken(c.Context)
 
-	if terminalID, err := rdb.Get(ctx, RTerminal(token)).Int(); err != nil {
-		return c.NotFound(err.Error())
-	} else if terminalID == form.ID {
+	terminalMap := make(map[int]string)
+	if err := GetV4Redis(RUser(userID.(int)), &terminalMap); err != nil {
+		return c.ServiceUnavailable(err.Error())
+	}
+
+	var currentTerminalID int
+	for k, v := range terminalMap {
+		if v == token {
+			currentTerminalID = k
+			break
+		}
+	}
+	if 0 == currentTerminalID {
+		return c.Unauthorized("The current terminal is not authorized")
+	}
+
+	if currentTerminalID == form.ID {
+		// 终端不能在有其他终端存在时，删除自己，所以
+		// 当请求删除的终端 ID 与当前操作终端的 ID 相同时，拒绝删除请求，
+		// 除非当前账号有且只有一个终端，即当前操作终端时，删除有效。
 		count, err := client.Terminal.
 			Query().
 			Where(terminal.HasUserWith(user.ID(userID.(int)))).
@@ -34,7 +52,19 @@ func deleteTerminal(c *Context) error {
 		}
 	}
 
-	if err := rdb.Del(ctx, RToken(token)).Err(); err != nil {
+	deleteTerminalToken := terminalMap[form.ID]
+	delete(terminalMap, form.ID)
+	d := rdb.TTL(ctx, RUser(userID.(int))).Val()
+
+	_, err := rdb.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		if err1 := SetV2RedisPipe(pipe, RUser(userID.(int)), &terminalMap, d); err1 != nil {
+			return err1
+		}
+		pipe.Del(ctx, RToken(deleteTerminalToken))
+		return nil
+	})
+
+	if err != nil {
 		return c.InternalServerError(err.Error())
 	}
 
