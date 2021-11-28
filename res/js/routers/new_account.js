6a9062a7bd0531c27ca46d5ea2c12f77cbe86c3d
excing <courage.exc@gmail.com>
2021-11-28 16:22:32 +0800

    Test webauthn registration

--- res/js/routers/new_account.js
+++ res/js/routers/new_account.js
@@ -37,12 +37,48 @@ const NewAccount = {
     },
 
     makeCredential(state, challenge) {
-      axios({
-        method: "PUT",
-        url: queryRestful("/v1/account/t/makeCredential", { state: state, challenge: challenge })
-      }).then(function (resp) {
-        authSuccess(resp.data)
-      }).catch(function (err) { })
+      // axios({
+      //   method: "PUT",
+      //   url: queryRestful("/v1/account/t/makeCredential", { state: state, challenge: challenge })
+      // }).then(function (resp) {
+      //   authSuccess(resp.data)
+      // }).catch(function (err) { })
+      putBeginRegistration(challenge, state, (resp) => {
+        this.requestWebAuthn(state, challenge, resp.data)
+      }, this.beginRegistrationFailure)
+    },
+
+    beginRegistrationFailure(err) {
+      this.toast(err, "error")
+    },
+    requestWebAuthn(state, challenge, makeCredentialOptions) {
+      makeCredentialOptions.publicKey.challenge = bufferDecode(makeCredentialOptions.publicKey.challenge);
+      makeCredentialOptions.publicKey.user.id = bufferDecode(makeCredentialOptions.publicKey.user.id);
+      if (makeCredentialOptions.publicKey.excludeCredentials) {
+        for (var i = 0; i < makeCredentialOptions.publicKey.excludeCredentials.length; i++) {
+          makeCredentialOptions.publicKey.excludeCredentials[i].id = bufferDecode(makeCredentialOptions.publicKey.excludeCredentials[i].id);
+        }
+      }
+      // console.log("Credential Creation Options");
+      // console.log(makeCredentialOptions);
+      navigator.credentials.create(makeCredentialOptions)
+        .then((newCredential) => {
+          console.log("PublicKeyCredential Created");
+          console.log(newCredential);
+          postFinishRegistration(challenge, state, newCredential, this.requestWebAuthnSuccess, this.requestWebAuthnFailure)
+        }).catch((err) => {
+          console.info(err);
+          this.toast(err, "error")
+        });
+    },
+
+    requestWebAuthnSuccess(resp) {
+      this.toast("Success", "success")
+      console.log(resp.data);
+    },
+    requestWebAuthnFailure(err) {
+      this.toast("Error " + err, "error")
+      console.info(err);
     },
 
     createAccount() {
--- res/js/utils/rest.js
+++ res/js/utils/rest.js
@@ -1,5 +1,34 @@
 // rest.js
 
+function postFinishRegistration(challenge, state, newCredential, success, failure) {
+  let attestationObject = new Uint8Array(newCredential.response.attestationObject);
+  let clientDataJSON = new Uint8Array(newCredential.response.clientDataJSON);
+  let rawId = new Uint8Array(newCredential.rawId);
+
+  axios({
+    method: "POST",
+    url: queryRestful("/v1/account/t/finishRegistration", { state: state, challenge: challenge }),
+    data: JSON.stringify({
+      id: newCredential.id,
+      rawId: bufferEncode(rawId),
+      type: newCredential.type,
+      response: {
+        attestationObject: bufferEncode(attestationObject),
+        clientDataJSON: bufferEncode(clientDataJSON),
+      },
+    }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+  }).then(success).catch(failure)
+}
+
+function putBeginRegistration(challenge, state, success, failure) {
+  axios({
+    method: "PUT",
+    url: queryRestful("/v1/account/t/beginRegistration", { state: state, challenge: challenge })
+  }).then(success).catch(failure)
+}
+
 function putMakeCredential(challenge, state, success, failure) {
   axios({
     method: "PUT",
