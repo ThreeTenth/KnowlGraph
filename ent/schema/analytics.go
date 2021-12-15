e870d240ca183beef1a347638e7992ad72ad2693
excing <courage.exc@gmail.com>
2021-12-15 20:28:12 +0800

    Update: Set a separate field for the version number.

--- ent/schema/analytics.go
+++ ent/schema/analytics.go
@@ -33,18 +33,23 @@ func (Analytics) Fields() []ent.Field {
 		field.String("lang").Default(""),
 		// 设备（型号和类型，PC 或 phone 或 pad）
 		// 设备分辨率
-		// 系统（操作系统）
-		// 平台（Web 或 移动应用程序或 PC 应用或其他，如手表、物联网设备等）
-		// 浏览器（Chrome 或 edge 或客户端）
+		// 系统（操作系统，和版本号）
+		// 平台（Web 或 移动应用程序或 PC 应用或其他，如手表、物联网设备等，以及他们的版本号）
+		// 浏览器（Chrome 或 edge 或客户端，以及他们的版本号）
 		field.String("device").Default(""),
 		field.String("device_type").Default(""),
 		field.String("display").Default(""),
 		field.String("os").Default(""),
+		field.String("os_version").Default(""),
 		field.String("platform").Default(""),
+		field.String("platform_version").Default(""),
 		field.String("browser").Default(""),
+		field.String("browser_version").Default(""),
+		// 统计数据接口版本号
 		field.Int("version"),
-		field.Time("start_time"),
-		field.Time("created_at").Default(time.Now),
+		// 发起统计时间
+		field.Time("start_time").StructTag(`json:"-"`),
+		field.Time("created_at").StructTag(`json:"-"`).Default(time.Now),
 	}
 }
 
