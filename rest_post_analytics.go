4f9ad871f8bef10a1d79e545ac07ebda5bdcc215
excing <courage.exc@gmail.com>
2021-12-15 12:28:28 +0800

    Add: post analytics restful api

---
+++ rest_post_analytics.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"net/url"
+	"time"
+
+	ua "github.com/mileusna/useragent"
+)
+
+func postAnalytics(c *Context) error {
+	var body struct {
+		Event      string    `json:"event" binding:"required"`
+		Category   string    `json:"category" binding:"required"`
+		Label      string    `json:"label" binding:"required"`
+		Message    string    `json:"message"`
+		City       string    `json:"city"`
+		Timezone   string    `json:"timezone"`
+		Referrer   string    `json:"referrer" binding:"required"`
+		URL        string    `json:"url"`
+		Path       string    `json:"path"`
+		Lang       string    `json:"lang"`
+		Device     string    `json:"device"`
+		DeviceType string    `json:"device_type"`
+		Display    string    `json:"display"`
+		Os         string    `json:"os"`
+		Platform   string    `json:"platform" binding:"required"`
+		Browser    string    `json:"browser"`
+		Version    int       `json:"version" binding:"required"`
+		StartTime  time.Time `json:"start_time" binding:"required"`
+	}
+
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		return c.BadRequest(err.Error())
+	}
+
+	ua := ua.Parse(c.Request.UserAgent())
+
+	if body.URL == "" {
+		body.URL = c.Request.Referer()
+	}
+	if body.Path == "" {
+		if _url, err := url.Parse(body.URL); err == nil {
+			body.Path = _url.Path
+		}
+	}
+	if body.Browser == "" {
+		body.Browser = ua.Name + " " + ua.Version
+	}
+	if body.Lang == "" {
+		body.Lang = c.UserLang()
+	}
+	if body.Os == "" {
+		body.Os = ua.OS + " " + ua.OSVersion
+	}
+	if body.Device == "" {
+		body.Device = ua.Device
+	}
+	if body.DeviceType == "" {
+		if ua.Mobile {
+			body.DeviceType = "Mobile"
+		} else if ua.Tablet {
+			body.DeviceType = "Tablet"
+		} else if ua.Desktop {
+			body.DeviceType = "Desktop"
+		} else {
+			body.DeviceType = "Unknown"
+		}
+	}
+	if body.Display == "" {
+		body.Display = "Unknown"
+	}
+	if body.City == "" {
+		ipinfo, _ := ForeignIP(c.ClientIP())
+		if ipinfo != nil {
+			body.City = ipinfo.City
+			body.Timezone = ipinfo.Timezone
+		}
+	}
+
+	analyticsCreater := client.Analytics.Create().
+		SetEvent(body.Event).
+		SetCategory(body.Category).
+		SetLabel(body.Label).
+		SetMessage(body.Message).
+		SetReferrer(body.Referrer).
+		SetURL(body.URL).
+		SetPath(body.Path).
+		SetLang(body.Lang).
+		SetCity(body.City).
+		SetTimezone(body.Timezone).
+		SetOs(body.Os).
+		SetPlatform(body.Platform).
+		SetBrowser(body.Browser).
+		SetDevice(body.Device).
+		SetDeviceType(body.DeviceType).
+		SetDisplay(body.Display).
+		SetStartTime(body.StartTime).
+		SetVersion(1)
+
+	if userID, ok := c.Get(GinKeyUserID); ok {
+		analyticsCreater.SetUserID(userID.(int))
+	}
+
+	analytics, err := analyticsCreater.Save(ctx)
+	if err != nil {
+		return c.InternalServerError(err.Error())
+	}
+	return c.Ok(analytics)
+}
