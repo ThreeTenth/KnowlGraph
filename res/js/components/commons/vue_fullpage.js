007bb8fb6b5eddcfe7cb4ff8c68576344d986451
excing <courage.exc@gmail.com>
2022-01-18 14:48:17 +0800

    Update: Only arrow keys are supported.

--- res/js/components/commons/vue_fullpage.js
+++ res/js/components/commons/vue_fullpage.js
@@ -4,74 +4,81 @@ Vue.component('fullpage', {
 
   data: function () {
     return {
-      scrollTimer: null,
-      scrollTime: new Date(),
       elements: [],
-      isIntercept: false,
     }
   },
 
   methods: {
-    scrollStart(e) {
-    },
-    scrollDo(e) {
-      if (this.isIntercept) return
-      if (0 < e.deltaY) {
-        // down
-
-        let startElement = indexStartAtWindow(this.elements)
-        if (startElement.overflow) {
-          if (e.deltaY < startElement.offset) {
-            window.scrollBy(0, e.deltaY)
-          } else {
-            this.isIntercept = true
-            window.scrollBy(0, startElement.offset + ScrollYOffset)
-          }
+    downMove(deltaY, behavior = "smooth") {
+      let startElement = indexStartAtWindow(this.elements)
+      if (startElement.overflow) {
+        if (deltaY < startElement.offset) {
+          window.scrollBy({ top: deltaY, behavior: behavior })
         } else {
-          this.isIntercept = true
-          this.elements[startElement.index + 1].scrollIntoView({ behavior: "smooth" })
+          window.scrollBy({ top: startElement.offset + ScrollYOffset, behavior: behavior })
         }
-      } else if (e.deltaY < 0) {
-        // up
-
-        let endElement = indexEndAtWindow(this.elements)
-        if (endElement.overflow) {
-          if (e.deltaY < -endElement.offset) {
-            window.scrollBy(0, e.deltaY)
-          } else {
-            this.isIntercept = true
-            window.scrollBy(0, -endElement.offset)
-          }
-        } else {
-          this.isIntercept = true
-          this.elements[endElement.index - 1].scrollIntoView({ behavior: "smooth" })
-        }
-      }
-    },
-    scrollEnd(e) {
-      console.log("scroll end");
-      this.isIntercept = false
-    },
-
-    mousewhell(e) {
-      e.preventDefault()
-      window.clearTimeout(this.scrollTimer)
-      this.scrollTimer = window.setTimeout(() => {
-        this.scrollEnd(e)
-      }, 300)
-      if (300 < new Date() - this.scrollTime) {
-        this.scrollStart(e)
       } else {
-        this.scrollDo(e)
+        let pageIndex = getValueInRange(startElement.index + 1, 0, this.elements.length)
+        this.elements[pageIndex].scrollIntoView({ behavior: "smooth" })
       }
-      this.scrollTime = new Date()
+    },
+
+    upMove(deltaY, behavior = "smooth") {
+      let endElement = indexEndAtWindow(this.elements)
+      if (endElement.overflow) {
+        if (-endElement.offset < deltaY) {
+          window.scrollBy({ top: deltaY, behavior: behavior })
+        } else {
+          window.scrollBy({ top: -endElement.offset - ScrollYOffset, behavior: behavior })
+        }
+      } else {
+        let pageIndex = getValueInRange(endElement.index - 1, 0, this.elements.length)
+        this.elements[pageIndex].scrollIntoView({ behavior: "smooth" })
+      }
+    },
+
+    keydownHandle(e) {
+      if (e.altKey && (e.keyCode == 37 || e.keyCode == 39)) {
+        // 浏览器前进后退的默认行为
+        return
+      }
+      switch (e.keyCode) {
+        case 9:  // Tab
+        case 35:  // End
+        case 36:  // Home
+          // 禁止 Tab, Home 和 End 键的使用。
+          // 此功能会导致阅读不完整。
+          e.preventDefault()
+          break
+        case 33:  // 上页
+          e.preventDefault()
+          this.upMove(-(window.getBodyHeight()))
+          break
+        case 34:  // 下页
+          e.preventDefault()
+          this.downMove(window.getBodyHeight())
+          break
+        case 37:  // 左
+        case 38:  // 上
+          e.preventDefault()
+          this.upMove(-200)
+          break
+        case 32:  // 空格键
+        case 39:  // 右
+        case 40:  // 下
+          e.preventDefault()
+          this.downMove(200)
+          break
+
+        default:
+          return
+      }
     },
   },
 
   created() {
-    document.addEventListener('scroll', this.scrollHandle, { passive: false })
-    document.addEventListener('touchmove', this.scrollHandle, { passive: false });  //passive 参数不能省略，用来兼容ios和android
-    document.addEventListener('wheel', this.mousewhell, { passive: false })
+    document.addEventListener('keydown', this.keydownHandle, { passive: false })    //passive 参数不能省略，用来兼容ios和android
+
     this.$nextTick(() => {
       let slots = this.$slots["default"]
       for (let index = 0; index < slots.length; index++) {
@@ -84,9 +91,7 @@ Vue.component('fullpage', {
   },
 
   beforeDestroy() {
-    document.removeEventListener('scroll', this.scrollHandle)
-    document.removeEventListener('touchmove', this.scrollHandle)
-    document.removeEventListener('wheel', this.mousewhell)
+    document.removeEventListener('keydown', this.keydownHandle)
   },
 
   template: com_fullpage,
