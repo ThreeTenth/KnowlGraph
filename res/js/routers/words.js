a88179e83da9986d0267d5be9ebac5c030e020ad
excing <courage.exc@gmail.com>
2022-02-24 16:07:44 +0800

    Update: Categorizing words.

--- res/js/routers/words.js
+++ res/js/routers/words.js
@@ -1,5 +1,25 @@
 // words.js
 
 const Words = {
+  computed: {
+    publicWords() {
+      if (!this.user.words) return null
+      var pws = []
+      this.user.words.forEach(word => {
+        if (word.status == 'public')
+          pws.push(word)
+      })
+      return pws.length == 0 ? null : pws
+    },
+    yourPrivateWords() {
+      if (!this.user.words) return null
+      var pws = []
+      this.user.words.forEach(word => {
+        if (word.status == 'private')
+          pws.push(word)
+      })
+      return pws.length == 0 ? null : pws
+    },
+  },
   template: fgm_words,
 }
